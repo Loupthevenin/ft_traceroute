2f9bb757e29b7e96645eebe8162e7fee3b465d86
Loupthevenin <loup.thevenin.1@gmail.com>
2025-07-23 10:34:20 +0200

    start traceroute

---
+++ includes/ft_traceroute.h
@@ -0,0 +1,7 @@
+#ifndef FT_TRACEROUTE_H
+# define FT_TRACEROUTE_H
+
+# include "ft_printf.h"
+# include "libft.h"
+
+#endif
---
+++ srcs/ft_traceroute.c
@@ -0,0 +1,9 @@
+#include "../includes/ft_traceroute.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_printf("Hello world !");
+	return (0);
+}
